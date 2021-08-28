3575372bdd9389cb9e16d92f6e44a735401e7a7f
Grigorii Dvoeglazov <dvoegor@gmail.com>
2021-08-28 23:32:03 +0400

    added both endpoints with comments

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const moment = require('moment');
+
 const knex = require('knex')({
   client: 'pg',
   connection: {
@@ -10,6 +11,14 @@ const knex = require('knex')({
     port: 5432,
   },
 });
+/* 
+  Ниже представлены названия таблицы БД и ее столбцы с их типами
+*/
+// gps_coordinates
+// id int,
+// latitude float4,
+// longitude float4,
+// created_at timestamp default now()
 
 const app = express();
 const PORT = 3000;
@@ -20,6 +29,68 @@ app.use(
   })
 );
 app.use(express.json());
+
+/* 
+  На этот эндпоинт мы принимаем ID пользователя и его текущие GPS координаты,
+  распарсиваем их и все данные записываем в БД.
+*/
+// {
+//   "userId": "1",
+//   "gps": "40.741895,-73.989308"
+// }
+app.post('/send-gps-data', (req, res) => {
+  const userId = req.body.userId;
+  const gpsArr = req.body.gps.split(',');
+  const latitude = gpsArr[0];
+  const longitude = gpsArr[1];
+  knex('gps_coordinates')
+    .insert({ id: userId, latitude: latitude, longitude: longitude })
+    .then(() => {
+      res.status(200).send('Data is sent');
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
+
+/* 
+  На этот эндпоинт мы принимаем ID пользователя и его текущие GPS координаты,
+  распарсиваем их и все данные записываем в БД.
+*/
+// get-gps-data?userId=1&startDate=2021-08-27&endDate=2021-08-29
+app.get('/get-gps-data', (req, res) => {
+  const userId = req.query.userId;
+  const startDate = req.query.startDate ? moment(req.query.startDate).format() : false;
+  const endDate = req.query.endDate ? moment(req.query.endDate).format() : false;
+  knex('gps_coordinates')
+  .select('latitude', 'longitude')
+  .where('id', userId)
+  .modify(function (queryBuilder) {
+    if (startDate) {
+      queryBuilder.where('created_at', '>=', startDate);
+    }
+  })
+  .modify(function (queryBuilder) {
+    if (endDate) {
+      queryBuilder.where('created_at', '<=', endDate);
+    }
+  })
+  .then((data) => {
+    res.status(200).send(data);
+    /* 
+    Output:
+    */
+    // [
+    //   {
+    //       "latitude": 40.741894,
+    //       "longitude": -73.98931
+    //   }
+    // ]
+  })
+  .catch((err) => {
+    res.status(500).send(err);
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}.`);
